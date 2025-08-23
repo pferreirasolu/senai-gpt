1a2ebac1acf4f6c7371b98b3f53fc50990024c0a
Pferreirasolu <pedrohg.ferreira@hotmail.com>
2025-08-22 21:48:44 -0300

    inicialização das condicionais em javascript

---
+++ javascript/script.js
@@ -0,0 +1,30 @@
+
+
+// Exercicio 1 
+
+// let nome = prompt("Qual é seu nome?");
+// let idade = prompt("Qual é sua idade:")
+
+// console.log("Meu nome é " +nome+", e minha idade é "+idade+".");
+// alert("Meu nome é " +nome+", e minha idade é "+idade+".");
+
+
+let idade = prompt("Qual é a idade:")
+
+if (idade < 18) {
+
+    alert("Cuidado, você é menor de idade")
+} else {
+    alert("Parabéns, você é maior de idade")
+
+};
+
+// let numero =prompt("Digite um número!");
+
+// if (numero > 0) {
+// alert("O numero é positivo!")
+
+// } else {
+
+//     alert("O número é negativo!")
+// }
